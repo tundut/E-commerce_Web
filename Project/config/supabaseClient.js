bec3fbcc80da297726d6a73a723fc730e06144e6
tundut <ducnguyenphan05@gmail.com>
2025-12-10 19:17:56 +0700

    fix supabase config

--- Project/config/supabaseClient.js
+++ Project/config/supabaseClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://vgknrfmxjholecjxccwj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZna25yZm14amhvbGVjanhjY3dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NTI1NTAsImV4cCI6MjA2MjMyODU1MH0.EQyAh3a_SoEv87VhPtpLjAqoYbvfUa2pfZUXzhQzBuU'; // <-- Dán key mới vào đây
+const supabaseUrl = 'https://lzrlwykwshqdrtjgbhea.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cmx3eWt3c2hxZHJ0amdiaGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNTUwMDksImV4cCI6MjA4MDkzMTAwOX0.rbLCHhko5jNLkb_kvIO4fPXYUMthG7Sn2Ar133LDdBc'; // <-- Dán key mới vào đây
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
